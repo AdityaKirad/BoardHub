ecac073d9ffa85eb556dc62cccac1f3ed5fc7923
Aditya <kiradaditya2001@gmail.com>
2026-09-12 14:46:57 +0530

    feat: add Netlify plugins for enhanced routing and deployment support

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,15 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import { reactRouterDevTools } from "react-router-devtools";
+import netlifyReactRouter from "@netlify/vite-plugin-react-router";
 
 export default defineConfig({
-  plugins: [reactRouterDevTools(), reactRouter(), tailwindcss()],
+  plugins: [
+    reactRouterDevTools(),
+    reactRouter(),
+    tailwindcss(),
+    netlifyReactRouter(),
+  ],
   resolve: {
     tsconfigPaths: true,
   },
